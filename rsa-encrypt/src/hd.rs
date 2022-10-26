f90055a564021d4812f0d3ca29eb0c0605384057
Eliel Nfinic <elielmathe@nfinic.com>
2022-10-26 10:36:25 +0200

    Adding log in

--- rsa-encrypt/src/hd.rs
+++ rsa-encrypt/src/hd.rs
@@ -7,6 +7,13 @@ pub fn generate_mnemonic() -> Mnemonic{
     Mnemonic::random(&mut OsRng,Default::default())
 }
 
+pub fn check_mnemonic(phrase : &str) -> Result<Mnemonic, &str>{
+    match Mnemonic::new(phrase, Default::default()){
+        Err(_) => Err("invalid mnemo"),
+        Ok(res) => Ok(res)
+    }
+}
+
 pub fn xprivate_key(mnemo : Mnemonic, password : &str) -> ExtendedPrivateKey<SigningKey>{
     let seed = mnemo.to_seed(password);
     XPrv::new(&seed).unwrap()
--- rsa-encrypt/src/lib.rs
+++ rsa-encrypt/src/lib.rs
@@ -12,8 +12,34 @@ pub fn get_mnemonic() -> String{
     String::from(mn.phrase())
 }
 
+#[wasm_bindgen]
+pub fn check_mnemonic(phrase : &str) -> String{
+    let check = hd::check_mnemonic(phrase);
+    match check{
+        Err(_) => String::from("invalid mnemo"),
+        Ok(mn) => String::from("valid mnemo")
+    }
+}
+
 
 #[wasm_bindgen]
 pub fn generate_keys() -> String{
+
+    //hd::xprivate_key(mnemo, password)
     String::from("Eliel")
+}
+
+#[cfg(test)]
+mod tests{
+    #[test]
+    fn test_valid_mnemo(){
+        let check = super::check_mnemonic("rib rifle salmon witness survey expose consider harbor develop merit creek calm crumble fiscal tragic concert market salute brisk typical bundle clip pilot candy");
+        assert_eq!(check, String::from("valid mnemo"));
+    }
+
+    #[test]
+    fn test_invalid_mnemo(){
+        let check = super::check_mnemonic("fsdkjfsda");
+        assert_eq!(check, String::from("invalid mnemo"));
+    }
 }
