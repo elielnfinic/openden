aa8b011faf4784ed07572ebae947f9c174130c6e
Eliel Nfinic <elielmathe@nfinic.com>
2022-11-24 15:14:41 +0200

    Adding email submit to the back-end

--- backend/src/contact.rs
+++ backend/src/contact.rs
@@ -1,8 +1,15 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 use mongodb::{Client, options::ClientOptions};
 use mongodb::bson::{doc, Document};
+use serde::{Serialize,Deserialize};
+use serde_json;
 
 use dotenv;
+
+#[derive(Deserialize)]
+struct ContactFormObj{
+    email : String
+}
 
 async fn connect_db() -> Client{
     let mut options = ClientOptions::parse(dotenv::var("MONGO_URI").unwrap()).await.unwrap();
@@ -16,10 +23,8 @@ pub async fn add(req_body: String) -> impl Responder {
     let client = Client::with_uri_str(dotenv::var("MONGO_URI").unwrap()).await.unwrap();
     let db = client.database("openden");
     let collection = db.collection::<Document>("contacts");
-    collection.insert_many(vec![doc!{"email":"eliel@test.com"}],None).await.unwrap();
-    /*for db_name in db.list_database_names(None, None).await.unwrap() {
-        println!("{}", db_name);
-    }*/
+    let contact_obj : ContactFormObj = serde_json::from_str(&req_body).unwrap();
+    collection.insert_many(vec![doc!{"email":contact_obj.email.as_str()}],None).await.unwrap();
     println!("All is great");
     HttpResponse::Ok().body(req_body)
 }
